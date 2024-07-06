9a3c1c86cf653a4528a8b0000c60ea502620ec8b
Mateusz Skrzat <mateusz_skrzat@outlook.com>
2024-07-06 13:33:10 +0200

    Added Boss fight basic mechanics

--- Assets/Scripts/KingBehaviour.cs
+++ Assets/Scripts/KingBehaviour.cs
@@ -2,27 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum KingChildren
+{
+    shootingGameObject,
+    carousel
+}
+
 public class KingBehaviour : MonoBehaviour
 {
     public GameObject projectilePrefab;
     public GameObject playerGameObject;
+    public List<GameObject> livesGameObjects;
     Vector2 projectileInstantiationPosition;
     public float shootFrequency = 1f;
     public float phaseTwoShootFrequency = 1.5f;
     public float projectileSpeed = 50f;
     public int phaseTwoProjectileQuantity = 5;
     public bool isAtPhaseTwo = false;
+    public int livesAmount = 4;
 
     // Start is called before the first frame update
     void Start()
     {
-        projectileInstantiationPosition = gameObject.transform.GetChild(0).transform.position;
+        projectileInstantiationPosition = gameObject.transform.GetChild((int)KingChildren.shootingGameObject).transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        gameObject.transform.GetChild((int)KingChildren.carousel).transform.Rotate(Vector3.forward * Time.deltaTime * 50f);
     }
 
     public void InstantiateProjectile(float delay = 0f)
@@ -48,8 +56,15 @@ public class KingBehaviour : MonoBehaviour
 
     }
 
-    public void DamageKing() {
-        Debug.Log("AGH, Damage");
+    public void DamageKing()
+    {
+        if (livesAmount > 0) {
+            livesGameObjects[livesAmount - 1].SetActive(false);
+            livesAmount--;
+        }
+        else {
+            GameManager.instance.GameOver();
+        }
     }
 
     public void PhaseTwoShoot()
