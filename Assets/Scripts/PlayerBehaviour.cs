1a5522a796df209304aa1f58cefa3b11eb69d54c
Mateusz Skrzat <mateusz_skrzat@outlook.com>
2024-07-06 02:48:15 +0200

    Added burn animation

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
+using Unity.Burst.Intrinsics;
 using Unity.VisualScripting;
 using UnityEditor.Search;
 using UnityEditor.ShaderGraph.Internal;
@@ -7,8 +9,11 @@ using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour
 {
-    Rigidbody2D rb = new Rigidbody2D();
-    Animator animator = new Animator();
+    Rigidbody2D rb;
+    Animator animator;
+
+    [SerializeField]
+    CinemachineVirtualCamera vCamera;
 
     [SerializeField]
     float speed = 10f;
@@ -17,44 +22,50 @@ public class PlayerBehaviour : MonoBehaviour
     float jumpForce = 20f;
 
     private float horizontalAxis = 0f;
+    public bool isDead = false;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        isDead = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //jumping
-        if (Input.GetButtonDown("Jump")) {
-            rb.AddForce(Vector2.up * jumpForce);
-        }
-
-        //moving left and right
         float newHorizontalAxis = Input.GetAxis("Horizontal") * speed;
-        if (newHorizontalAxis != horizontalAxis) {
-            rb.velocity = new Vector2(0, rb.velocity.y);
-        }
-        rb.AddForce(new Vector2(newHorizontalAxis, 0));
+        if (!isDead) 
+        {
+            //jumping
+            if (Input.GetButtonDown("Jump")) {
+                rb.AddForce(Vector2.up * jumpForce);
+            }
+            //moving left and right
+            if (newHorizontalAxis != horizontalAxis) {
+                rb.velocity = new Vector2(0, rb.velocity.y);
+            }
+            rb.AddForce(new Vector2(newHorizontalAxis, 0));
 
-        //animating movement
-        animator.SetFloat("HorizontalAxis", newHorizontalAxis);
-        if (newHorizontalAxis != 0) {
-            animator.SetBool("IsMoving", true);
-        }
-        else {
-            animator.SetBool("IsMoving", false);
-        }
-        
+            //animating movement
+            animator.SetFloat("HorizontalAxis", newHorizontalAxis);
+            if (newHorizontalAxis != 0) {
+                animator.SetBool("IsMoving", true);
+            }
+            else {
+                animator.SetBool("IsMoving", false);
+            }
+        }     
     }
 
     void OnTriggerEnter2D(Collider2D collider) 
     {
         if(collider.tag == "Light") 
         {
+            isDead = true;
+            animator.SetBool("IsDead", true);
+            vCamera.Follow = null;
             Debug.Log("GAME OVER SUCKER");
         }
     }
